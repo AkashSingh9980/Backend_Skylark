b522880bf7c53bbbc08bf8ed4ddf206260db51ef
rittmang <ritomgupta99@gmail.com>
2020-07-21 21:02:31 +0530

    Get contact page to work

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -98,26 +98,27 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 			<div class="mail_form">
 				<h3 class="tittle-w3ls">Send Us a Message</h3>
 				<div class="inner_sec_info_wthree_agile">
-					<form action="#" method="post">
+					<form action="{{url('contactus')}}" method="post">
+						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 						<span class="input input--chisato">
-						<input class="input__field input__field--chisato" name="Name" type="text" id="input-13" placeholder=" " required="" />
+						<input class="input__field input__field--chisato" name="visitor_name" type="text" id="input-13" placeholder=" " required="" />
 						<label class="input__label input__label--chisato" for="input-13">
 							<span class="input__label-content input__label-content--chisato" data-content="Name">Name</span>
 						</label>
 						</span>
 						<span class="input input--chisato">
-						<input class="input__field input__field--chisato" name="Email" type="email" id="input-14" placeholder=" " required="" />
+						<input class="input__field input__field--chisato" name="visitor_email" type="email" id="input-14" placeholder=" " required="" />
 						<label class="input__label input__label--chisato" for="input-14">
 							<span class="input__label-content input__label-content--chisato" data-content="Email">Email</span>
 						</label>
 						</span>
 						<span class="input input--chisato">
-						<input class="input__field input__field--chisato" name="Subject" type="text" id="input-15" placeholder=" " required="" />
+						<input class="input__field input__field--chisato" name="visitor_subject" type="text" id="input-15" placeholder=" " required="" />
 						<label class="input__label input__label--chisato" for="input-15">
 							<span class="input__label-content input__label-content--chisato" data-content="Subject">Subject</span>
 						</label>
 						</span>
-						<textarea name="Message" placeholder="Your comment here..." required=""></textarea>
+						<textarea name="visitor_message" placeholder="Your comment here..." required=""></textarea>
 						<input type="submit" value="Submit">
 					</form>
 
