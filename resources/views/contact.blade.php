562dfe2206ce90fdd6678aae0e067b341524b2b8
rittmang <ritomgupta99@gmail.com>
2020-07-21 22:17:56 +0530

    fix session text

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -30,55 +30,52 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 	<link href="css/contact.css" rel="stylesheet" type="text/css" media="all" />
 	<link href="css/font-awesome.css" rel="stylesheet">
 	<!-- //for bootstrap working -->
-	<link href="//fonts.googleapis.com/css?family=Raleway:100,100i,200,300,300i,400,400i,500,500i,600,600i,700,800"
-		rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Raleway:100,100i,200,300,300i,400,400i,500,500i,600,600i,700,800" rel="stylesheet">
 	<link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,400,400i,600,600i,700" rel="stylesheet">
 </head>
 
 <body>
-	<div class="top_header" id="home">
-		<!-- Fixed navbar -->
-		<nav class="navbar navbar-default navbar-fixed-top">
-			<div class="nav_top_fx_w3ls_agileinfo">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
-						aria-expanded="false" aria-controls="navbar">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<div class="logo-w3layouts-agileits">
-						<h1> <a class="navbar-brand" href="index.html"><i class="fa fa-clone" aria-hidden="true"></i>
-								SKYLARK GROUP <span class="desc">AIM FOR SKY</span></a></h1>
+<div class="top_header" id="home">
+	<!-- Fixed navbar -->
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="nav_top_fx_w3ls_agileinfo">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false"
+				    aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+				<div class="logo-w3layouts-agileits">
+					<h1> <a class="navbar-brand" href="index.html"><i class="fa fa-clone" aria-hidden="true"></i> SKYLARK GROUP <span class="desc">AIM FOR SKY</span></a></h1>
 
-					</div>
 				</div>
-				<!--<div id="navbar" class="navbar-collapse collapse">
+			</div>
+			<!--<div id="navbar" class="navbar-collapse collapse">
 				<div class="nav_right_top">
 					<ul class="nav navbar-nav navbar-right">
 						<li><a class="request" href="contact.html">Send Request</a></li>
 
 					</ul>-->
-				<ul class="nav navbar-nav">
-					<li><a href="index.html">Home</a></li>
-					<li><a href="gallery.html">Gallery</a></li>
-					<li><a href="career.html">Career</a></li>
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-							aria-expanded="false">Our Services <span class="caret"></span></a>
-						<ul class="dropdown-menu">
-							<li><a href="packages.html">Packages</a></li>
-							<li><a href="product.html">Products</a></li>
-							<li><a href="services.html">Services</a>
-						</ul>
-					<li class="active"><a href="contact.html">Contact</a></li>
-					</li>
-				</ul>
+					<ul class="nav navbar-nav">
+						<li><a href="index.html">Home</a></li>
+						<li><a href="gallery.html">Gallery</a></li>
+						<li><a href="career.html">Career</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Our Services <span class="caret"></span></a>
+							<ul class="dropdown-menu">
+								    <li><a href="packages.html">Packages</a></li>
+                                    <li><a href="product.html">Products</a></li>
+									<li><a href="services.html">Services</a>
+							</ul>
+							<li class="active"><a href="contact.html">Contact</a></li>
+						</li>
+					</ul>
+				</div>
 			</div>
-	</div>
-	<!--/.nav-collapse -->
-	</div>
+			<!--/.nav-collapse -->
+		</div>
 	</nav>
 	</div>
 	<!--/banner_info-->
@@ -95,85 +92,81 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 	</div>
 	<!--//banner_info-->
 	<!-- /inner_content -->
-	<div class="banner_bottom">
+<div class="banner_bottom">
 		<div class="container">
 			<div class="mail_form">
+				<h3 class="tittle-w3ls">Send Us a Message</h3>
 				@if(session('status'))
-					<h3>
-						{{ session('status') }}
-					</h3>
-				@else
-					<h3 class="tittle-w3ls">Send Us a Message</h3>
 					<div class="inner_sec_info_wthree_agile">
-						<form action="{{ url('contact') }}" method="post">
-							<input type="hidden" name="_token" value="{{ csrf_token() }}">
-							<span class="input input--chisato">
-								<input class="input__field input__field--chisato" name="visitor_name" type="text"
-									id="input-13" placeholder=" " required="" />
+						<h3>
+							{{session('status')}}
+						</h3>
+					</div>
+				@else
+						<div class="inner_sec_info_wthree_agile">
+							<form action="{{url('contact')}}" method="post">
+								<input type="hidden" name="_token" value="{{ csrf_token() }}">
+								<span class="input input--chisato">
+								<input class="input__field input__field--chisato" name="visitor_name" type="text" id="input-13" placeholder=" " required="" />
 								<label class="input__label input__label--chisato" for="input-13">
-									<span class="input__label-content input__label-content--chisato"
-										data-content="Name">Name</span>
+									<span class="input__label-content input__label-content--chisato" data-content="Name">Name</span>
 								</label>
-							</span>
-							<span class="input input--chisato">
-								<input class="input__field input__field--chisato" name="visitor_email" type="email"
-									id="input-14" placeholder=" " required="" />
+								</span>
+								<span class="input input--chisato">
+								<input class="input__field input__field--chisato" name="visitor_email" type="email" id="input-14" placeholder=" " required="" />
 								<label class="input__label input__label--chisato" for="input-14">
-									<span class="input__label-content input__label-content--chisato"
-										data-content="Email">Email</span>
+									<span class="input__label-content input__label-content--chisato" data-content="Email">Email</span>
 								</label>
-							</span>
-							<span class="input input--chisato">
-								<input class="input__field input__field--chisato" name="visitor_subject" type="text"
-									id="input-15" placeholder=" " required="" />
+								</span>
+								<span class="input input--chisato">
+								<input class="input__field input__field--chisato" name="visitor_subject" type="text" id="input-15" placeholder=" " required="" />
 								<label class="input__label input__label--chisato" for="input-15">
-									<span class="input__label-content input__label-content--chisato"
-										data-content="Subject">Subject</span>
+									<span class="input__label-content input__label-content--chisato" data-content="Subject">Subject</span>
 								</label>
-							</span>
-							<textarea name="visitor_message" placeholder="Your comment here..." required=""></textarea>
-							<input type="submit" value="Submit">
-						</form>
+								</span>
+								<textarea name="visitor_message" placeholder="Your comment here..." required=""></textarea>
+								<input type="submit" value="Submit">
+							</form>
 
-					</div>
-			</div>
-		</div>
-
-		<div class="inner_sec_info_wthree_agile">
-			<div class="col-md-8 map">
-				<iframe
-					src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387142.84010033106!2d-74.25819252532891!3d40.70583163828471!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew+York%2C+NY%2C+USA!5e0!3m2!1sen!2sin!4v1475140387172"
-					style="border:0"></iframe>
-			</div>
-			<div class="col-md-4 contact_grids_info">
-				<div class="contact_grid">
-					<div class="contact_grid_right">
-						<h4> OFFICE 1</h4>
-						<p>435 City hall,</p>
-						<p>NewYork City SD092.</p>
-					</div>
+						</div>
+					</div>	
 				</div>
-				<div class="contact_grid">
-					<div class="contact_grid_right">
-						<h4> OFFICE 2</h4>
-						<p>8088 Jasmine hall,</p>
-						<p>NewYork City SD092.</p>
-					</div>
+				@endif
+			
+			<div class="inner_sec_info_wthree_agile">
+           	<div class="col-md-8 map">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387142.84010033106!2d-74.25819252532891!3d40.70583163828471!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew+York%2C+NY%2C+USA!5e0!3m2!1sen!2sin!4v1475140387172"
+					    style="border:0"></iframe>
 				</div>
-				<div class="contact_grid" data-aos="flip-up">
+				<div class="col-md-4 contact_grids_info">
+					<div class="contact_grid">
+						<div class="contact_grid_right">
+							<h4> OFFICE 1</h4>
+							<p>435 City hall,</p>
+							<p>NewYork City SD092.</p>
+						</div>
+					</div>
+					<div class="contact_grid">
+						<div class="contact_grid_right">
+							<h4> OFFICE 2</h4>
+							<p>8088 Jasmine hall,</p>
+							<p>NewYork City SD092.</p>
+						</div>
+					</div>
+					<div class="contact_grid" data-aos="flip-up">
 
-					<div class="contact_grid_right">
-						<h4>OFFICE 3</h4>
-						<p>436 Honey hall,</p>
-						<p>NewYork City SD092.</p>
+						<div class="contact_grid_right">
+							<h4>OFFICE 3</h4>
+							<p>436 Honey hall,</p>
+							<p>NewYork City SD092.</p>
+						</div>
+						<div class="clearfix"> </div>
 					</div>
 					<div class="clearfix"> </div>
 				</div>
 				<div class="clearfix"> </div>
 			</div>
-			<div class="clearfix"> </div>
 		</div>
-	</div>
 	</div>
 
 	<!-- footer -->
@@ -181,8 +174,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 		<div class="footer_inner_info_w3ls_agileits">
 			<div class="col-md-3 footer-left">
 				<h2><a href="index.html"><i class="fa fa-clone" aria-hidden="true"></i> SKYLARK GROUP </a></h2>
-				<p>Lorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora.
-				</p>
+				<p>Lorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora.</p>
 				<ul class="social-nav model-3d-0 footer-social social two">
 					<li>
 						<a href="#" class="facebook">
@@ -218,31 +210,22 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 							<li><a href="index.html">Home</a></li>
 							<li><a href="about.html">About</a></li>
 							<li><a href="404.html">Services</a></li>
-							<li><a href="signin.html">Signin</a></li>
+						    <li><a href="signin.html">Signin</a></li>
 							<li><a href="contact.html">Contact</a></li>
 						</ul>
 					</div>
 					<div class="col-md-3 sign-gd flickr-post">
 						<h4>Our <span>Works</span></h4>
 						<ul>
-							<li><a href="single.html"><img src="images/g1.jpg" alt=" " class="img-responsive" /></a>
-							</li>
-							<li><a href="single.html"><img src="images/g2.jpg" alt=" " class="img-responsive" /></a>
-							</li>
-							<li><a href="single.html"><img src="images/g3.jpg" alt=" " class="img-responsive" /></a>
-							</li>
-							<li><a href="single.html"><img src="images/g4.jpg" alt=" " class="img-responsive" /></a>
-							</li>
-							<li><a href="single.html"><img src="images/g5.jpg" alt=" " class="img-responsive" /></a>
-							</li>
-							<li><a href="single.html"><img src="images/g6.jpg" alt=" " class="img-responsive" /></a>
-							</li>
-							<li><a href="single.html"><img src="images/g7.jpg" alt=" " class="img-responsive" /></a>
-							</li>
-							<li><a href="single.html"><img src="images/g8.jpg" alt=" " class="img-responsive" /></a>
-							</li>
-							<li><a href="single.html"><img src="images/g4.jpg" alt=" " class="img-responsive" /></a>
-							</li>
+							<li><a href="single.html"><img src="images/g1.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single.html"><img src="images/g2.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single.html"><img src="images/g3.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single.html"><img src="images/g4.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single.html"><img src="images/g5.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single.html"><img src="images/g6.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single.html"><img src="images/g7.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single.html"><img src="images/g8.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single.html"><img src="images/g4.jpg" alt=" " class="img-responsive" /></a></li>
 						</ul>
 					</div>
 					<div class="col-md-5 sign-gd-two">
@@ -288,8 +271,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 			</div>
 			<div class="clearfix"></div>
 
-			<p class="copy-right">&copy 2018 Conceit. All rights reserved | Design by <a
-					href="http://w3layouts.com/">W3layouts</a></p>
+			<p class="copy-right">&copy 2018 Conceit. All rights reserved | Design by <a href="http://w3layouts.com/">W3layouts</a></p>
 		</div>
 	</div>
 	</div>
@@ -355,8 +337,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 		});
 	</script>
 
-	<a href="#home" class="scroll" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;">
-		</span></a>
+	<a href="#home" class="scroll" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
 
 
 
