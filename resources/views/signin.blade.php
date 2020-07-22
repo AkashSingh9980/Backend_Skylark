73464e0b57cad890faa1b2450a6bc6c73e9fd584
AkshitaMaskara <66301296+AkshitaMaskara@users.noreply.github.com>
2020-07-22 23:15:55 +0530

    Update signin.blade.php

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -47,27 +47,27 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
             <span class="icon-bar"></span>
           </button>
 				<div class="logo-w3layouts-agileits">
-					<h1> <a class="navbar-brand" href="index.html"><i class="fa fa-clone" aria-hidden="true"></i> Conceit <span class="desc">For your Business</span></a></h1>
+					<h1> <a class="navbar-brand" href="index"><i class="fa fa-clone" aria-hidden="true"></i> Conceit <span class="desc">For your Business</span></a></h1>
 				</div>
 			</div>
 			<div id="navbar" class="navbar-collapse collapse">
 				<div class="nav_right_top">
 					<ul class="nav navbar-nav navbar-right">
-						<li><a class="request" href="contact.html">Send Request</a></li>
+						<li><a class="request" href="contact">Send Request</a></li>
 
 					</ul>
 					<ul class="nav navbar-nav">
 						<li><a href="index.html">Home</a></li>
 						
-						<li><a href="portfolio.html">Portfolio</a></li>
-						<li><a href="contact.html">Contact</a></li>
-						<li class="active"><a href="gallery.html">Gallery</a></li>
+						<li><a href="portfolio">Portfolio</a></li>
+						<li><a href="contact">Contact</a></li>
+						<li class="active"><a href="gallery">Gallery</a></li>
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Our Services <span class="caret"></span></a>
 							<ul class="dropdown-menu">
-								<li><a href="packages.html">Packages</a></li>
-									<li><a href="products.html">Products</a></li>
-									<li><a href="services.html">Services</a></li>
+								<li><a href="packages">Packages</a></li>
+									<li><a href="products">Products</a></li>
+									<li><a href="services">Services</a></li>
 
 							</ul>
 						</li>
@@ -85,7 +85,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 		<div class="inner_breadcrumb">
 
 			<ul class="short">
-				<li><a href="index.html">Home</a><span>|</span></li>
+				<li><a href="index">Home</a><span>|</span></li>
 				<li>Signin</li>
 			</ul>
 		</div>
@@ -115,7 +115,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 							<li><a href="#"><i class="fa fa-rss"></i></a></li>
 						</ul>
 					</div>
-					<p><a href="signup.html"> Don't have an account?</a></p>
+					<p><a href="signup"> Don't have an account?</a></p>
 				</div>
 			</div>
 		</div>
@@ -125,7 +125,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 	<div class="footer">
 		<div class="footer_inner_info_w3ls_agileits">
 			<div class="col-md-3 footer-left">
-				<h2><a href="index.html"><i class="fa fa-clone" aria-hidden="true"></i> Conceit </a></h2>
+				<h2><a href="index"><i class="fa fa-clone" aria-hidden="true"></i> Conceit </a></h2>
 				<p>Lorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora.</p>
 				<ul class="social-nav model-3d-0 footer-social social two">
 					<li>
@@ -159,25 +159,25 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 					<div class="col-md-4 sign-gd">
 						<h4>Latest <span>Info</span> </h4>
 						<ul>
-							<li><a href="index.html">Home</a></li>
-							<li><a href="about.html">About</a></li>
-							<li><a href="404.html">Services</a></li>
-							<li><a href="signin.html">Signin</a></li>
-							<li><a href="contact.html">Contact</a></li>
+							<li><a href="index">Home</a></li>
+							<li><a href="about">About</a></li>
+							<li><a href="404">Services</a></li>
+							<li><a href="signin">Signin</a></li>
+							<li><a href="contact">Contact</a></li>
 						</ul>
 					</div>
 					<div class="col-md-3 sign-gd flickr-post">
 						<h4>Our <span>Works</span></h4>
 						<ul>
-							<li><a href="single.html"><img src="images/g1.jpg" alt=" " class="img-responsive" /></a></li>
-							<li><a href="single.html"><img src="images/g2.jpg" alt=" " class="img-responsive" /></a></li>
-							<li><a href="single.html"><img src="images/g3.jpg" alt=" " class="img-responsive" /></a></li>
-							<li><a href="single.html"><img src="images/g4.jpg" alt=" " class="img-responsive" /></a></li>
-							<li><a href="single.html"><img src="images/g5.jpg" alt=" " class="img-responsive" /></a></li>
-							<li><a href="single.html"><img src="images/g6.jpg" alt=" " class="img-responsive" /></a></li>
-							<li><a href="single.html"><img src="images/g7.jpg" alt=" " class="img-responsive" /></a></li>
-							<li><a href="single.html"><img src="images/g8.jpg" alt=" " class="img-responsive" /></a></li>
-							<li><a href="single.html"><img src="images/g4.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single"><img src="images/g1.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single"><img src="images/g2.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single"><img src="images/g3.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single"><img src="images/g4.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single"><img src="images/g5.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single"><img src="images/g6.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single"><img src="images/g7.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single"><img src="images/g8.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single"><img src="images/g4.jpg" alt=" " class="img-responsive" /></a></li>
 						</ul>
 					</div>
 					<div class="col-md-5 sign-gd-two">
