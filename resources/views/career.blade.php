ab600cc23467efcf00a7c0fcb38c8738eaf6511d
AkshitaMaskara <66301296+AkshitaMaskara@users.noreply.github.com>
2020-07-22 22:21:01 +0530

    Update career.blade.php

--- resources/views/career.blade.php
+++ resources/views/career.blade.php
@@ -58,18 +58,18 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 
 						</ul>
 						<ul class="nav navbar-nav">
-							<li><a href="index.html">Home</a></li>
-							<li><a href="gallery.html">Gallery</a></li>
-							<li class="active" ><a href="career.html">Career</a></li>
+							<li><a href="index">Home</a></li>
+							<li><a href="gallery">Gallery</a></li>
+							<li class="active" ><a href="career">Career</a></li>
 
 							<li class="dropdown">
 								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Our Services<span class="caret"></span></a>
 								<ul class="dropdown-menu">
-									<li><a href="packages.html">Packages</a></li>
-									<li><a href="product.html">Products</a></li>
-									<li><a href="services.html">Services</a></li>
+									<li><a href="packages">Packages</a></li>
+									<li><a href="product">Products</a></li>
+									<li><a href="services">Services</a></li>
 								</ul>
-								<li><a href="contact.html">Contact</a></li>
+								<li><a href="contact">Contact</a></li>
 							</li>
 						</ul>
 					</div>
@@ -85,7 +85,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 		<div class="inner_breadcrumb">
 
 			<ul class="short">
-				<li><a href="index.html">Home</a><span>|</span></li>
+				<li><a href="index">Home</a><span>|</span></li>
 				<li>Career</li>
 			</ul>
 		</div>
@@ -168,7 +168,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 	<div class="footer">
 		<div class="footer_inner_info_w3ls_agileits">
 			<div class="col-md-3 footer-left">
-				<h2><a href="index.html"><i class="fa fa-clone" aria-hidden="true"></i>SKYLARK GROUP </a></h2>
+				<h2><a href="index"><i class="fa fa-clone" aria-hidden="true"></i>SKYLARK GROUP </a></h2>
 				<p>Lorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora.</p>
 				<ul class="social-nav model-3d-0 footer-social social two">
 					<li>
@@ -202,26 +202,26 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 					<div class="col-md-4 sign-gd">
 						<h4>Latest <span>Info</span> </h4>
 						<ul>
-							<li><a href="index.html">Home</a></li>
-							<li><a href="about.html">About</a></li>
-							<li><a href="404.html">Services</a></li>
-							<li><a href="signin.html">Signin</a></li>
-							<li><a href="contact.html">Contact</a></li>
-							<li><a href="career.html">Career</a><li>						
+							<li><a href="index">Home</a></li>
+							<li><a href="about">About</a></li>
+							<li><a href="404">Services</a></li>
+							<li><a href="signin">Signin</a></li>
+							<li><a href="contact">Contact</a></li>
+							<li><a href="career">Career</a><li>						
 							</ul>
 					</div>
 					<div class="col-md-3 sign-gd flickr-post">
 						<h4>Our <span>Works</span></h4>
 						<ul>
-							<li><a href="single.html"><img src="images/g1.jpg" alt=" " class="img-responsive" /></a></li>
-							<li><a href="single.html"><img src="images/g2.jpg" alt=" " class="img-responsive" /></a></li>
-							<li><a href="single.html"><img src="images/g3.jpg" alt=" " class="img-responsive" /></a></li>
-							<li><a href="single.html"><img src="images/g4.jpg" alt=" " class="img-responsive" /></a></li>
-							<li><a href="single.html"><img src="images/g5.jpg" alt=" " class="img-responsive" /></a></li>
-							<li><a href="single.html"><img src="images/g6.jpg" alt=" " class="img-responsive" /></a></li>
-							<li><a href="single.html"><img src="images/g7.jpg" alt=" " class="img-responsive" /></a></li>
-							<li><a href="single.html"><img src="images/g8.jpg" alt=" " class="img-responsive" /></a></li>
-							<li><a href="single.html"><img src="images/g4.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single"><img src="images/g1.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single"><img src="images/g2.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single"><img src="images/g3.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single"><img src="images/g4.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single"><img src="images/g5.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single"><img src="images/g6.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single"><img src="images/g7.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single"><img src="images/g8.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single"><img src="images/g4.jpg" alt=" " class="img-responsive" /></a></li>
 						</ul>
 					</div>
 					<div class="col-md-5 sign-gd-two">
